7b9c0d8b84a369d8e461817e6cd11db582193f08
Leelai Hayslett <Leelai.Hayslett@gmail.com>
2022-05-23 23:18:40 -0400

    HTMLrouting js file complete

--- Routes/htmlRouting.js
+++ Routes/htmlRouting.js
@@ -1,7 +1,14 @@
+const res = require("express/lib/response");
 const path = require ("path");
 
 module.exports = function(app){
 
-    app.get("/")
+    app.get("/", function (req, res){
+        res.sendFile(path.join(__dirname, "/../public/index.html"))
+    });
+
+    app.get("/notes",function(req,res){
+        res.sendFile(path.join(__dirname, "/../public/notes.html"));
+    });
 
 }
